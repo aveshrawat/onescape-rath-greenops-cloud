94848b3f792345cd5cdddb8ac6acfb8e6fbb7d57
aveshrawat <avesh.rawat@onescape.in>
2026-05-09 16:20:46 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RathGreenOpsCloud from "./rath/RathGreenOpsCloud.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RathGreenOpsCloud />
+  </React.StrictMode>
+);
